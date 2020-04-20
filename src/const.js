fa845fd9fe4c0bfa5c6200748148a1225e4b168c
2and1or3 <2and1or3@gmail.com>
2020-04-20 12:53:58 +0300

    add NoTask

--- src/const.js
+++ src/const.js
@@ -16,4 +16,13 @@ const MONTH_NAMES = [
 const DAYS = [`mo`, `tu`, `we`, `th`, `fr`, `sa`, `su`];
 const COLORS = [`black`, `green`, `yellow`, `pink`, `blue`];
 
-export {MONTH_NAMES, DAYS, COLORS};
+const FILTER_TYPES = {
+  ALL: `all`,
+  OVERDUE: `overdue`,
+  TODAY: `today`,
+  FAVORITES: `favorites`,
+  REPEATING: `repeating`,
+  ARCHIVE: `archive`,
+};
+
+export {MONTH_NAMES, DAYS, COLORS, FILTER_TYPES};
